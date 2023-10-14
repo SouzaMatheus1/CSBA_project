390248500c2a6ce73a9c22fc4625cb630335f6bc
Matheus Souza <99676439+SouzaMatheus1@users.noreply.github.com>
2023-10-13 21:16:53 -0300

    Final

--- app/classes.py
+++ app/classes.py
@@ -1,34 +1,37 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.spinner import Spinner
-from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.button import Button
+from kivy.core.window   import Window
 from kivy.uix.gridlayout import GridLayout
 
-class PedidoApp(App):
-    def build(self):
+class PizzaPapaApp(BoxLayout):
+    def __init__(self, **kwargs):
+        super(PizzaPapaApp, self).__init__(**kwargs)
+        Window.size = (1000,1000)
+        self.retiraPedido()
+
+    def retiraPedido(self):
         self.layout = BoxLayout(orientation='vertical', padding=10)
         
         # NOME PIZZARIA
         self.nome_estabelecimento = Label(text='Pizza do Papa')
         self.layout.add_widget(self.nome_estabelecimento)
         
-        # NOME CCLIENTE
+        # NOME CLIENTE
         self.nome_usuario = TextInput(hint_text='Digite seu nome', multiline=False)
         self.layout.add_widget(self.nome_usuario)
         
         # SABOR PIZZA
-        self.opcoes_sabores = Spinner(text='Escolha um sabor',
-                                              values=('Quatro queijos', 'Mussarela', 'Portuguesa', 'Calabresa', 'Frango catupiry', 'Crocante'))
+        self.opcoes_sabores = Spinner(values=('Quatro queijos', 'Mussarela', 'Portuguesa', 'Calabresa', 'Frango catupiry', 'Crocante'), text='Escolha um sabor')
         self.layout.add_widget(self.opcoes_sabores)
-        self.pizzas_tradicionais    = ['Quatro queijos', 'Mussarela', 'Calabresa']
-        self.pizzas_especiais       = ['Portuguesa', 'Frango catupiry', 'Crocante']
-
+        self.pizzas_tradicionais = ['Quatro queijos', 'Mussarela', 'Calabresa']
+        
         # TAMANHO PIZZA
-        self.opcoes_tamanhos = Spinner(text='Escolha o tamanho',
-                                      values=('Broto', 'Média', 'Grande', 'Gigante'))
+        self.opcoes_tamanhos = Spinner(text='Escolha o tamanho', values=('Broto', 'Média', 'Grande', 'Gigante'))
         self.layout.add_widget(self.opcoes_tamanhos)
         self.pizzas_tamanhos = ['Broto', 'Média', 'Grande', 'Gigante']
         
@@ -39,7 +42,7 @@ class PedidoApp(App):
         self.layout.add_widget(self.quantidade)
         
         # GRID DE PAGAMENTOS
-        self.pagamento_layout = GridLayout(cols=4, padding=12)
+        self.pagamento_layout = GridLayout(cols=2, padding=12, spacing=10)
         
         self.pagamento_a_vista_esp = CheckBox(group='pagamento', active=False)
         self.pagamento_label_esp = Label(text='À vista (espécie)')
@@ -78,11 +81,11 @@ class PedidoApp(App):
         
         # SAIR APLICAÇÃO
         self.sair_button = Button(text='Sair', size_hint_y=None, height=40)
-        self.sair_button.bind(on_press=self.stop)
+        self.sair_button.bind(on_press=self.parar_app)
         self.layout.add_widget(self.sair_button)
-        
-        return self.layout
-    
+
+        self.add_widget(self.layout)
+
     def calcular_total(self, instance):
         valor_total = 0
 
@@ -135,8 +138,14 @@ class PedidoApp(App):
         self.nome_usuario.text = ''
         self.opcoes_sabores.text = 'Escolha um sabor'
         self.quantidade.text = ''
-        self.tipo_pagamento.text = 'Escolha o tipo de pagamento'
         self.resultado.text = ''
 
-if __name__ == '__main__':
-    PedidoApp().run()
+    def parar_app(self):
+        App.get_running_app().stop()
+
+class PedidoApp(App):
+    def build(self):
+        return PizzaPapaApp()
+
+app = PedidoApp()
+app.run()
